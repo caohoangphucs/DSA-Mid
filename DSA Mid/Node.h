780366d61a9eac6ea93d4677aa0af1c0a709a06c
Phuc <videorubik82>
2025-09-07 20:54:27 +0700

    double linked list node

--- DSA Mid/Node.h
+++ DSA Mid/Node.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 
+//For linked list
 template <typename T>
 struct Node {
     T data;           
@@ -10,8 +11,17 @@ struct Node {
 
     Node(const T& value) : data(value), next(nullptr) {}
 };
+//For Double linked list
+template <typename T>
+struct DNode {
+    T data;
+    DNode<T>* prev;
+    DNode<T>* next;
 
+    DNode(const T& value) : data(value), prev(nullptr), next(nullptr) {}
+};
 
+//For binary search tree
 template <typename T>
 struct TreeNode {
     T data;
@@ -21,3 +31,4 @@ struct TreeNode {
     TreeNode() : data(T()), left(nullptr), right(nullptr) {}
     TreeNode(const T& value) : data(value), left(nullptr), right(nullptr) {}
 };
+
